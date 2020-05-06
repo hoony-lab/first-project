b323c1d9652d3289aa95ababd90ba0a4c42403f8
hoony.kim <jihun9838@naver.com>
2020-05-06 15:01:53 +0900

    test.java first line annotation

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 class Test{
 	public static void main(String[] args){
 
-		System.out.println("Hell_o world");
+//		System.out.println("Hell_o world");
 		System.out.println("Hell world");
 		// g
 	}
