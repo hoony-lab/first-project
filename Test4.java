6e11e9b007fd6f73b2fec7685e7bc7961fff1e01
hoony.kim <jihun9838@naver.com>
2020-05-06 12:59:26 +0900

    ???asdha haha ahah

--- Test4.java
+++ Test4.java
@@ -1,6 +1,8 @@
 class Test4.java{
 	public static void main(String[] args){
 		System.out.println("HEGEGEGEG");
+		// 613e2ccbd5812c02c20ced697a77c67b8c43d882
+		// 613e2ccbd5812c02c20ced697a77c67b8c43d882
 	}
 }
 
